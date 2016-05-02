f787356bbcd554f8f83db3a61f2ddf2619924ac7
psndna88 <psndna88@gmail.com>
2016-05-02 19:32:37 +0530

    lowmemorykiller: Don't count swap cache pages twice
    The lowmem_shrink function discounts all the swap cache pages from
    the file cache count. The zone aware code also discounts all file
    cache pages from a certain zone.  This results in some swap cache
    pages being discounted twice, which can result in the low memory
    killer being unnecessarily aggressive.
    
    Fix the low memory killer to only discount the swap cache pages
    once.
    
    Change-Id: I650bbfbf0fbbabd01d82bdb3502b57ff59c3e14f
    Signed-off-by: Liam Mark <lmark@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	drivers/staging/android/lowmemorykiller.c

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -742,6 +742,10 @@ const char * const vmstat_text[] = {
 #endif
 	"nr_anon_transparent_hugepages",
 	"nr_free_cma",
+	"nr_swapcache",
+	"nr_ion",
+	"nr_ion_pool",
+	"nr_ion_cma",
 	"nr_dirty_threshold",
 	"nr_dirty_background_threshold",
 
