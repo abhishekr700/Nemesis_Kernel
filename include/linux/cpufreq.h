51038c2f1d3805d794ee43162f5f90770c4a1caa
Abhishek Ranjan <abhishekr700@gmail.com>
2016-05-13 10:10:30 +0530

    Added InteractiveX Governer

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -444,6 +444,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactiveX;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactiveX)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
