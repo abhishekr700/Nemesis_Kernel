24db39dbde125c3645989aadda477189d90a69be
Abhishek Ranjan <abhishekr700@gmail.com>
2016-07-22 21:25:44 +0530

    Minor fix

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1402,19 +1402,15 @@ static inline void inc_nr_running(struct rq *rq)
 
 static inline void dec_nr_running(struct rq *rq)
 {
-<<<<<<< HEAD
 	#ifdef CONFIG_INTELLI_PLUG
 	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
 	#endif
-	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
+	sched_update_nr_prod(cpu_of(rq), 1, false);
 	#ifdef CONFIG_INTELLI_PLUG
 	write_seqcount_begin(&nr_stats->ave_seqcnt);
 	nr_stats->ave_nr_running = do_avg_nr_running(rq);
 	nr_stats->nr_last_stamp = rq->clock_task;
 	#endif
-=======
-	sched_update_nr_prod(cpu_of(rq), 1, false);
->>>>>>> 28a2edd... sched: Fix bug in average nr_running and nr_iowait calculation
 	rq->nr_running--;
 	#ifdef CONFIG_INTELLI_PLUG
  	write_seqcount_end(&nr_stats->ave_seqcnt);
